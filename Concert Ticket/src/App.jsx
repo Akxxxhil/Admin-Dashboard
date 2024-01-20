4d8e9715c0a511a7c06a39268a8cf0e7c056c9b5
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-01-20 19:33:46 +0530

    project started

---
+++ Concert Ticket/src/App.jsx
@@ -0,0 +1,12 @@
+
+import Website_name from "./Components/Website_Name/Website_name";
+
+function App() {
+    return (
+        <>
+            <Website_name />
+        </>
+    );
+}
+
+export default App;
