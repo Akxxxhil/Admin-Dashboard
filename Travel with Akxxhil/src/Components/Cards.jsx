be7ebaa6a1746128a9e8f2e0d894e085b0d35923
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-03-13 00:10:52 +0530

    updated

--- Travel with Akxxhil/src/Components/Cards.jsx
+++ Travel with Akxxhil/src/Components/Cards.jsx
@@ -1,34 +1,45 @@
 import { useState } from "react";
-import './Cards.css'
+import "./Cards.css";
 
-function Cards({ id, name, info, image, price,notIntrested }) {
+function Cards({ id, name, info, image, price, notIntrested }) {
+  const [readMore, setReadMore] = useState(false);
+  const [color, setColor] = useState("black");
+  const description = readMore ? info : `${info.substring(0, 100)}...`;
 
-  const [readMore,setReadMore]=useState(false)
-  const description=readMore ?info :`${info.substring(0,100)}...`
-
-  function readmorehandler(){
-    setReadMore(!readMore)
+  function readmorehandler() {
+    setReadMore(!readMore);
+  }
+  function favChanger() {
+    setColor(color === "black" ? "pink" : "black");
   }
   return (
-    
-      <div className="cards-all">
-        <div className="tour-image">
-          <img src={image} alt="" />
-        </div>
-        <div>
-          <div className="tour-price">{price}</div>
-          <div className="tour-name">{name}</div>
-        </div>
-        <div className="tour-details">
-          {description}
-          <span className="Read-more" onClick={readmorehandler}>
-            {readMore ? 'Show Less':`Read More`}
-          </span>
-        </div>
-
-        <button className="NotIntrested" onClick={()=>{notIntrested(id)}}>Not Intrested</button>
+    <div className="cards-all">
+      <div className="tour-image">
+        <img src={image} alt="" />
       </div>
-    
+      <div>
+        <div className="tour-price">{price}</div>
+        <div className="tour-name">{name}</div>
+      </div>
+      <div className="tour-details">
+        {description}
+        <span className="Read-more" onClick={readmorehandler}>
+          {readMore ? "Show Less" : `Read More`}
+        </span>
+      </div>
+
+      <div className="ButtonsofBottom">
+        <button
+          className="NotIntrested"
+          onClick={() => {
+            notIntrested(id);
+          }}
+        >
+          Not Intrested
+        </button>
+        <button style={{ backgroundColor: color }} onClick={favChanger}>Fav</button>
+      </div>
+    </div>
   );
 }
 export default Cards;
